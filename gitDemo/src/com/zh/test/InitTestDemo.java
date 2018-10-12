fda7fa87e1de0ea5230eb3a74796a9997be1365f
xsyl08 <xsyl08@126.com>
2018-10-12 17:23:03 +0800

    111

--- gitDemo/src/com/zh/test/InitTestDemo.java
+++ gitDemo/src/com/zh/test/InitTestDemo.java
@@ -8,16 +8,24 @@ package com.zh.test;
 public class InitTestDemo {
 	private static int i;
 	private static Integer integer;
+	private static double d;
+	private static Double doub;
 	private static String string;
-	
+
 	public static void main(String[] args) {
 		int i_1;
 		Integer integer_1;
+		double d_1;
+		double doub_1;
 		String string_1;
-		System.out.println("i="+i+", integer="+integer+", string="+string);
+		System.out.println("i="+i+", integer="+integer);
+		System.out.println("d="+d+", doub="+doub);
+		System.out.println("string="+string);
 		//System.out.println(i_1);
 		//System.out.println(integer_1);
-		//System.out.println(s1);
+		//System.out.println(d_1);
+		//System.out.println(doub_1);
+		//System.out.println(string_1);
 	}
 
 }
